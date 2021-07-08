f3f62f0e69fdd3ed2a9f74bd95b63f2a731356c8
Pilar89 <86802151+Pilar89@users.noreply.github.com>
2021-07-08 15:40:40 -0500

    new API donations (#6)
    new database and API donations added

--- src/New_API/my_API.py
+++ src/New_API/my_API.py
@@ -22,7 +22,27 @@ def query_people():
     return(file)
 
 
-def convert_to_dictionary(file):
+def query_donations():
+    cur = cnn.cursor()
+    cur.execute(
+        """select personas.nombre, personas.cedula, sum(donaciones.valor) total_donaciones 
+        from personas, donaciones 
+        where personas.cedula = donaciones.cedula 
+        group by personas.cedula, personas.nombre"""
+
+    )
+    data = cur.fetchall()
+
+    file = []
+    for row in data:
+        list = []
+        for i in row:
+            list.append(i)
+        file.append(list)
+    return(file)
+
+
+def convert_to_dictionary_people(file):
 
     list = []
     for i in file:
@@ -30,6 +50,19 @@ def convert_to_dictionary(file):
         dictionary["nombre"] = i[0]
         dictionary["cedula"] = i[1]
         dictionary["telefono"] = i[2]
+        list.append(dictionary)
+
+    return list
+
+
+def convert_to_dictionary_donations(file):
+
+    list = []
+    for i in file:
+        dictionary = {}
+        dictionary["nombre"] = i[0]
+        dictionary["cedula"] = i[1]
+        dictionary["valor"] = int(i[2])
         list.append(dictionary)
 
     return list
@@ -45,7 +78,13 @@ def Index():
 
 @app.route("/persons")
 def persons():
-    file = convert_to_dictionary(query_people())
+    file = convert_to_dictionary_people(query_people())
+    return Response(json.dumps(file), mimetype='application/json')
+
+
+@app.route("/donations")
+def donations():
+    file = convert_to_dictionary_donations(query_donations())
     return Response(json.dumps(file), mimetype='application/json')
 
 
