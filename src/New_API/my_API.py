74c96c8502dacd0f7c3e62afd664bcd9ab364a69
Pilar Coral <ingeniera.coral@gmail.com>
2021-07-08 00:14:33 -0500

    donations database is added

--- src/New_API/my_API.py
+++ src/New_API/my_API.py
@@ -22,6 +22,26 @@ def query_people():
     return(file)
 
 
+def query_donations():
+    cur = cnn.cursor()
+    cur.execute(
+        """select personas.nombre, personas.cedula, sum(donaciones.valor) total_donaciones 
+        from personas, donaciones 
+        where personas.cedula = donaciones.cedula 
+        group by personas.cedula, personas.nombre"""
+
+    )
+    data = cur.fetchall()
+
+    file = []
+    for row in data:
+        list = []
+        for i in row:
+            list.append(i)
+        file.append(list)
+    return(file)
+
+
 def convert_to_dictionary(file):
 
     list = []
@@ -30,6 +50,19 @@ def convert_to_dictionary(file):
         dictionary["nombre"] = i[0]
         dictionary["cedula"] = i[1]
         dictionary["telefono"] = i[2]
+        list.append(dictionary)
+
+    return list
+
+
+def convert_to_dictionary1(file):
+
+    list = []
+    for i in file:
+        dictionary = {}
+        dictionary["nombre"] = i[0]
+        dictionary["cedula"] = i[1]
+        dictionary["valor"] = int(i[2])
         list.append(dictionary)
 
     return list
@@ -46,6 +79,12 @@ def Index():
 @app.route("/persons")
 def persons():
     file = convert_to_dictionary(query_people())
+    return Response(json.dumps(file), mimetype='application/json')
+
+
+@app.route("/donations")
+def donations():
+    file = convert_to_dictionary1(query_donations())
     return Response(json.dumps(file), mimetype='application/json')
 
 
