b815e8bf6c8a41227c7b565c3dd315685b60cfbd
Pilar Coral <ingeniera.coral@gmail.com>
2021-07-08 00:14:07 -0500

    donations database is added

--- src/bases_de_datos.py
+++ src/bases_de_datos.py
@@ -1,45 +1,93 @@
 import mysql.connector
-cnn = mysql.connector.connect(host = "localhost", 
-                              user = "pilar", 
-                              passwd = "pilar", 
-                              database = "database1")
+cnn = mysql.connector.connect(host="localhost",
+                              user="pilar",
+                              passwd="pilar",
+                              database="database1")
 
-def consultar_personas(): 
-    cur = cnn.cursor() 
-    cur.execute("select * from personas") 
+
+def consultar_personas():
+    cur = cnn.cursor()
+    cur.execute("select * from personas")
     datos = cur.fetchall()
-      
-    for fila in datos: 
+
+    for fila in datos:
         print(fila)
-           
+
+
 def insertar_nueva_persona(nombre, cedula, telefono):
-    cur = cnn.cursor() 
-    sql = """insert into personas (nombre,cedula,telefono)
-        values (%s, %s, %s)"""
+    cur = cnn.cursor()
+    sql = """insert into personas (nombre,cedula,telefono)   
+    values (%s, %s, %s)"""
     datos = (nombre, cedula, telefono)
-    cur.execute(sql, datos) 
+    cur.execute(sql, datos)
     cnn.commit()
-   
+
+
+def insertar_donaciones(cedula, valor, fecha):
+    cur = cnn.cursor()
+    sql = """insert into donaciones (cedula,valor,fecha)
+        values (%s, %s, %s)"""
+    datos = (cedula, valor, fecha)
+    cur.execute(sql, datos)
+    cnn.commit()
+
+
+def consultar_donaciones():
+    cur = cnn.cursor()
+    cur.execute(
+        """select personas.nombre, personas.cedula, sum(donaciones.valor) total_donaciones 
+        from personas, donaciones 
+        where personas.cedula = donaciones.cedula 
+        group by personas.cedula, personas.nombre"""
+    )
+    datos = cur.fetchall()
+    for fila in datos:
+        print(fila)
+
+
 def crear_menu():
     while True:
-        
+
         menu = int(input("""Usted tiene el siguiente menu 
-        1 para agrepar persona 
-        2 para mostrar personas 
-        0 para salir 
-        Escriba una opcion: """))
-        
+                         1 para agregar persona 
+                         2 para mostrar personas 
+                         3 para agregar registro de donacion
+                         4 para consultar registro de donaciones
+                         0 para salir 
+                         Escriba una opcion: """))
+
         if menu == 1:
             nombre = input("Ingrese el nombre de la persona:")
             cedula = input("Ingrese la cedula: ")
             telefono = input("Ingrese el telefono: ")
-            insertar_nueva_persona(nombre, cedula, telefono)
-            print("Se ha agregado los cambios exitosamente" )
-            
+            try:
+                insertar_nueva_persona(nombre, cedula, telefono)
+                print("Se ha agregado los cambios exitosamente")
+
+            except:
+                print("Oops!  El valor esta duplicado.  intente de nuevo...")
+                break
+
         if menu == 2:
             consultar_personas()
-            
+
+        if menu == 3:
+            cedula = input("Ingrese la cedula: ")
+            valor = int(input("Ingrese el valor de la donacion: "))
+            fecha = input("Ingrese la fecha de la donacion A/M/D: ")
+            try:
+                insertar_donaciones(cedula, valor, fecha)
+                print("Se ha agregado los cambios exitosamente")
+
+            except:
+                print("Oops!  La cedula no esta registrada.  intente de nuevo...")
+                break
+
+        if menu == 4:
+            consultar_donaciones()
+
         if menu == 0:
             break
-            
+
+
 crear_menu()
